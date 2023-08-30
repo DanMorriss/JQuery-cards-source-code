5086bec35c1d3d7ed8a860ca30fb595fd2210e0c
Daniel Morriss <danielmorriss1@gmail.com>
2023-08-30 13:50:49 +0100

    Add challenges

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
  
 
- 	$("#stream1_btn").click(function() {
- 		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream1").addClass('highlight_stream');
+ 	$("#stream1_btn").on(function() {
+ 		$(".stream1").toggle();
+		$(".stream1").toggle(1000);
 	});
 	$("#stream2_btn").on("click", function() {
 		$(".stream1").removeClass('highlight_stream');
@@ -22,3 +20,51 @@ $(document).ready(function() {
 
 
 }); 
+
+// Challenge 1
+
+$('p').click(function() {
+    $('p').css('color', 'red');
+});
+
+$('h2').hover(function() {
+    $('h2').css('background', 'lightblue');
+});
+
+$('.card-panel').mouseenter(function() {
+    $('body').css('background-color', 'black');
+});
+
+$('.card-panel').mouseleave(function() {
+    $('body').css('background-color', '#e1e2e2');
+});
+
+// Challenge 2
+
+$('#card-panel-1').click(function() {
+    $('#card-panel-1').hide('slow');
+});
+
+$('#card-panel-2').click(function() {
+    $('#card-panel-2').hide('medium');
+});
+
+$('#card-panel-3').click(function() {
+    $('#card-panel-3').hide('fast');
+});
+
+$('#card-panel-4').click(function(){
+    $('#card-panel-4').hide(3000);
+});
+
+// Method Chaining challenge 1
+
+$('button').mouseenter(function(){
+    $('button').removeClass('make-red').addClass('make-border');
+});
+
+$('button').mouseleave(function(){
+    $('button').removeClass('make-border').addClass('make-red');
+});
+
+
